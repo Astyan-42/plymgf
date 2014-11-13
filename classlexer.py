5c8cf0ab1dd27f51abe960bb1ea4d98930e25f36
Astyan <astyan@omnos.fr>
2014-11-13 16:31:44 +0100

    proper but why lex seems not gready ?

---
+++ classlexer.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python
+
+import ply.lex as lex
+import ply.yacc as yacc
+
+import sys
+
+class Content:
+    
+    def __init__(self):
+        self.meta = { }
+        self.meta["charge"] = []
+        self.sentence = ""
+        self.glist = []
+
+class MGFLexer:
+    
+    def __init__(self):
+        self.lexer = lex.lex(module=self)
+    
+    head_tokens = ['CLE', 'PFA']
+    local_tokens = []
+    head_local_tokens = ['CHARGE']
+    other_tokens = ['EQUAL', 'COMMA',  'CHAR', 'INT', 'COMMENT', 'AND',
+    'CHARGE_VALUE']
+    
+    tokens = head_tokens+local_tokens+head_local_tokens+other_tokens
+    
+    #~ option only header
+    t_CLE = r"CLE"
+    t_PFA = r"PFA"
+    
+    #~ option only local
+    
+    #~ option header and local
+    t_CHARGE = r"CHARGE"
+    
+    #~ other tokens 
+    t_EQUAL = "="
+    t_COMMA = ","
+    t_CHAR = r"[^=,]"
+    t_INT = r"-{0,1}[0-9]+"
+    t_COMMENT = r"(\#){3}.*"
+    t_AND = "and"
+    t_CHARGE_VALUE = r"[0-9]+[+-]{1}"
+    
+    
+    t_ignore = '\t\n'
+
+    def t_error(self,t):
+        print "Illegal character '%s'" % t.value[0]
+        t.lexer.skip(1)
+        
+
+    def tokenize(self,data):
+        'Debug method!'
+        self.lexer.input(data)
+        while True:
+            tok = self.lexer.token()
+            if tok:
+                yield tok
+            else:
+                break
+
+class MGFParser:
+    
+    def __init__(self):
+        self.lexer = MGFLexer()
+        self.content = Content()
+        self.tokens = self.lexer.tokens
+        self.parser = yacc.yacc(module=self,write_tables=0,debug=False)
+
+    def parse(self,data):
+        if data:
+            return self.parser.parse(data,self.lexer.lexer,0,0,None)
+        else:
+            return []
+
+    def p_error(self,p):
+        print "Syntax error at '%s'" % p.value
+        exit(1)
+
+#~ COMMENT 
+
+    def p_statement_comment(self, p):
+        'statement : COMMENT'
+        pass
+
+#~ TERMINAL 
+    
+    def p_statement_cle(self, p):
+        'statement : CLE EQUAL sentence'
+        self.content.meta["cle"] = self.content.sentence
+        self.content.sentence = ""
+    
+    def p_statement_pfa(self, p):
+        'statement : PFA EQUAL INT'
+        self.content.meta["pfa"] = p[3]
+        
+    def p_statement_charge(self, p):
+        'statement : CHARGE EQUAL charges'
+        pass
+
+#~ NON TERMINAL 
+    
+    def p_sentence(self, p):
+        '''sentence : CHAR
+                    | CHAR sentence '''
+        self.content.sentence = p[1] + self.content.sentence
+        
+    def p_charges_add(self, p):
+        """charges : CHARGE_VALUE
+                   | CHARGE_VALUE CHAR AND CHAR charges
+                   | CHARGE_VALUE COMMA CHAR charges
+                   | INT
+                   | INT CHAR AND CHAR charges
+                   | INT COMMA CHAR charges"""
+        self.content.meta["charge"].append(p[1])
+        
+def main(argv):
+    parser = MGFParser()
+    try:
+        s = open("temp.mgf")
+    except EOFError:
+        pass
+    for line in s:
+        print line
+        parser.parse(line)
+    print parser.content.meta
+
+if __name__ == '__main__':
+    main(sys.argv)

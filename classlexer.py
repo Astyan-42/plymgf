d471fbad71b62c3520ed2644bbff42794c2681ea
Astyan <astyan@omnos.fr>
2015-01-29 15:41:41 +0100

    littles verifications added

--- classlexer.py
+++ classlexer.py
@@ -299,6 +299,18 @@ class MGFParser(object):
         print "Syntax error at '%s'" % p.value
         exit(1)
 
+    def in_header(self):
+        if self.content.inions != -1:
+            raise Exception
+        else:
+            return True
+    
+    def in_local(self):
+        if self.content.inions != 1:
+            raise Exception
+        else:
+            return True
+
 #~ COMMENT 
 
     def p_statement_comment(self, p):
@@ -311,48 +323,56 @@ class MGFParser(object):
 #~ option only header
     def p_statement_cle(self, p):
         'statement : CLE EQUAL sentence'
+        self.in_header()
         self.content.meta["cle"] = self.content.sentence
         self.content.sentence = ""
         print "CLE"
     
     def p_statement_com(self, p):
         'statement : COM EQUAL sentence'
+        self.in_header()
         self.content.meta["com"] = self.content.sentence
         self.content.sentence = ""
         print "COM"
     
     def p_statement_cutout(self, p):
         'statement : CUTOUT EQUAL list'
+        self.in_header()
         self.content.meta["cutout"] = self.content.glist
         self.content.glist = []
         print "CUTOUT"
     
     def p_statement_db(self, p):
         'statement : DB EQUAL sentence'
+        self.in_header()
         self.content.meta["db"] = self.content.sentence
         self.content.sentence = ""
         print "DB"
     
     def p_statement_decoy(self, p):
         'statement : DECOY EQUAL INT'
+        self.in_header()
         self.content.meta["decoy"] = p[3]
         self.content.sentence = ""
         print "DECOY"
     
     def p_statement_errortolerant(self, p):
         'statement : ERRORTOLERANT EQUAL INT'
+        self.in_header()
         self.content.meta["errortolerant"] = p[3]
         self.content.sentence = ""
         print "ERRORTOLERANT"
     
     def p_statement_format(self, p):
         'statement : FORMAT EQUAL sentence'
+        self.in_header()
         self.content.meta["format"] = self.content.sentence
         self.content.sentence = ""
         print "FORMAT"
     
     def p_statement_frames(self, p):
         'statement : FRAMES EQUAL list'
+        self.in_header()
         self.content.meta["frames"] = self.content.glist
         self.content.glist = []
         print "FRAMES"
@@ -360,49 +380,58 @@ class MGFParser(object):
     def p_statement_itol(self, p):
         '''statement : ITOL EQUAL INT
                      | ITOL EQUAL FLOAT'''
+        self.in_header()
         self.content.meta["itol"] = p[3]
         print "ITOL"
     
     def p_statement_itolu(self, p):
         'statement : ITOLU EQUAL sentence'
+        self.in_header()
         self.content.meta["itolu"] = self.content.sentence
         self.content.sentence = ""
         print "ITOLU"
     
     def p_statement_mass(self, p):
         'statement : MASS EQUAL sentence'
+        self.in_header()
         self.content.meta["mass"] = self.content.sentence
         self.content.sentence = ""
         print "MASS"
     
     def p_statement_mods(self, p):
         'statement : MODS EQUAL sentence'
+        self.in_header()
         self.content.meta["mods"] = self.content.sentence
         self.content.sentence = ""
         print "MODS"
         
     def p_statement_multi_site_mods(self, p):
         'statement : MULTI_SITE_MODS EQUAL INT'
+        self.in_header()
         self.content.meta["multi_site_mods"] = p[3]
         print "MULTI_SITE_MODS"
     
     def p_statement_pep_isotope_error(self, p):
         'statement : PEP_ISOTOPE_ERROR EQUAL INT'
+        self.in_header()
         self.content.meta["pep_isotope_error"] = p[3]
         print "PEP_ISOTOPE_ERROR"
     
     def p_statement_pfa(self, p):
         'statement : PFA EQUAL INT'
+        self.in_header()
         self.content.meta["pfa"] = p[3]
         print "PFA"
     
     def p_statement_precursor(self, p):
         'statement : PRECURSOR EQUAL FLOAT'
+        self.in_header()
         self.content.meta["precursor"] = p[3]
         print "PRECURSOR"
     
     def p_statement_quantification(self, p):
         'statement : QUANTITATION EQUAL sentence'
+        self.in_header()
         self.content.meta["quantitation"] = self.content.sentence
         self.content.sentence = ""
         print "QUANTITATION"
@@ -410,57 +439,69 @@ class MGFParser(object):
     def p_statement_report(self, p):
         '''statement : REPORT EQUAL AUTO
                      | REPORT EQUAL INT'''
+        self.in_header()
         self.content.meta["report"] = p[3]
         print "REPORT"
     
     def p_statement_reptype(self, p):
         '''statement : REPTYPE EQUAL sentence'''
+        self.in_header()
         self.content.meta["reptype"] = self.content.sentence
         self.content.sentence = ""
         print "REPTYPE"
     
     def p_statement_search(self, p):
         '''statement : SEARCH EQUAL sentence'''
+        self.in_header()
         self.content.meta["search"] = self.content.sentence
         self.content.sentence = ""
         print "SEARCH"
     
     def p_statement_seg(self, p):
         '''statement : SEG EQUAL FLOAT'''
+        self.in_header()
         self.content.meta["seg"] = p[3]
         print "SEG"
         
     def p_statement_taxonomy(self, p):
         '''statement : TAXONOMY EQUAL sentence'''
+        self.in_header()
         self.content.meta["taxonomy"] = self.content.sentence
         self.content.sentence = ""
         print "TAXONOMY"
         
     def p_statement_useremail(self, p):
         '''statement : USEREMAIL EQUAL sentence'''
+        self.in_header()
         self.content.meta["email"] = self.content.sentence
         self.content.sentence = ""
         print "USEREMAIL"
     
     def p_statement_username(self, p):
         '''statement : USERNAME EQUAL sentence'''
+        self.in_header()
         self.content.meta["username"] = self.content.sentence
         self.content.sentence = ""
         print "USERNAME"
     
     def p_statement_user(self, p):
         '''statement : USER INT EQUAL sentence'''
+        self.in_header()
         self.content.meta["user"+str(p[2])] = self.content.sentence
         self.content.sentence = ""
         print "USER"+str(p[2])
         
     def p_statement_begin_ions(self, p):
         '''statement : BEGIN CHAR IONS'''
+        self.in_header()
         self.content.inions = 1
         print "BEGIN_IONS"
-        
+    
+#~ option only local
+
     def p_statement_end_ions(self, p):
         '''statement : END CHAR IONS'''
+        self.in_local()
         self.content.ionsinfo['peaklist'] = self.content.peaklist
         self.content.ionslist.append(self.content.ionsinfo)
         self.content.peaklist = []
@@ -468,63 +509,71 @@ class MGFParser(object):
         self.content.inions = -1
         print "END_IONS"
     
-#~ option only local
-    
     def p_statement_comp(self, p):
         '''statement : COMP EQUAL sentence'''
+        self.in_local()
         self.content.ionsinfo["comp"] = self.content.sentence
         self.content.sentence = ""
         print "COMP"
     
     def p_statement_etag(self,p):
         'statement : ETAG EQUAL sentence'
+        self.in_local()
         self.content.ionsinfo["etag"] = self.content.sentence
         self.content.sentence = ""
         print "ETAG"
     
     def p_statement_locus(self, p):
         'statement : LOCUS EQUAL sentence'
+        self.in_local()
         self.content.ionsinfo["locus"] = self.content.sentence
         self.content.sentence = ""
         print "LOCUS"
     
     def p_statement_pepmass(self, p):
         'statement : PEPMASS EQUAL FLOAT'
+        self.in_local()
         self.content.ionsinfo["pepmass"] = p[3]
         print "PEPMASS"
     
     def p_statement_rawfile(self, p):
         'statement : RAWFILE EQUAL sentence'
+        self.in_local()
         self.content.ionsinfo["rawfile"] = self.content.sentence
         self.content.sentence = ""
         print "RAWFILE"
         
     def p_statement_rawscans(self, p):
         'statement : RAWSCANS EQUAL sentence'
+        self.in_local()
         self.content.ionsinfo["rawscans"] = self.content.sentence
         self.content.sentence = ""
         print "RAWSCANS"
     
     def p_statement_rtinseconds(self, p):
         'statement : RTINSECONDS EQUAL sentence'
+        self.in_local()
         self.content.ionsinfo["rtinseconds"] = self.content.sentence
         self.content.sentence = ""
         print "RTINSECONDS"
     
     def p_statement_seq(self, p):
         'statement : SEQ EQUAL sentence'
+        self.in_local()
         self.content.ionsinfo["seq"] = self.content.sentence
         self.content.sentence = ""
         print "SEQ"
     
     def p_statement_tag(self, p):
         'statement : TAG EQUAL sentence'
+        self.in_local()
         self.content.ionsinfo["tag"] = self.content.sentence
         self.content.sentence = ""
         print "TAG"
         
     def p_statement_title(self, p):
         'statement : TITLE EQUAL sentence'
+        self.in_local()
         self.content.ionsinfo["title"] = self.content.sentence
         self.content.sentence = ""
         print "TITLE"
@@ -533,6 +582,7 @@ class MGFParser(object):
         '''statement : FLOAT CHAR FLOAT CHAR INT
                      | FLOAT CHAR FLOAT CHAR CHARGE_VALUE
                      | FLOAT CHAR FLOAT'''
+        self.in_local()
         if len(p) == 6:
             self.content.peaklist.append((p[1], p[3], p[5]))
         else:
@@ -611,7 +661,7 @@ class MGFParser(object):
 def main(argv):
     parser = MGFParser()
     try:
-        s = open("temp.mgf")
+        s = open("temp2.mgf")
     except EOFError:
         pass
     for line in s:

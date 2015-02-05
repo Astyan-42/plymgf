62bb087126b789168d9087234281bfbbfa148d81
Astyan <astyan@omnos.fr>
2015-02-05 17:07:25 +0100

    correcting path

--- .script/allpath.py
+++ .script/allpath.py
@@ -5,7 +5,7 @@ import os
 lpath = []
 slpath = ""
 
-for osdir in os.walk("."):
+for osdir in os.walk("plymgf"):
     for fich in osdir[2]:
         if fich.split('.')[-1] == "py":
             lpath.append(osdir[0])
@@ -13,5 +13,16 @@ for osdir in os.walk("."):
 
 for path in lpath:
     slpath = slpath+os.path.join(path,"*.py")+" "
-    
+
+lpath = []    
+
+for osdir in os.walk("testu"):
+    for fich in osdir[2]:
+        if fich.split('.')[-1] == "py":
+            lpath.append(osdir[0])
+            break
+            
+for path in lpath:
+    slpath = slpath+os.path.join(path,"*.py")+" "
+
 print slpath
